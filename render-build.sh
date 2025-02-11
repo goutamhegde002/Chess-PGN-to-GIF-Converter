5b6b11f4b23792806c82687a4f4bd94787d5344d
M P GOUTAM <131381042+goutamhegde002@users.noreply.github.com>
2025-02-11 22:35:36 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Install system dependencies
 apt-get update && apt-get install -y libcairo2 libcairo2-dev
+
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
